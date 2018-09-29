11dea6fad3f8d3d95632dbfdbf5f65731f137e51
Bilal <chamgadarr@gmail.com>
2018-09-29 19:55:16 +0500

    Update index.el

--- index.el
+++ index.el
@@ -3,7 +3,7 @@
 (defvar *root* nil)              ;; root folder
 (defvar *projects* nil)        ;; projects list
 (defvar *adhoc-list* nil)
-
+(defvar *buffers* nil)
 (defvar *current-project* nil)
 
 
@@ -13,9 +13,71 @@
 	     (load-projects *root*))
     (message "directory  does not exist.")))
 
+;(eval-file (filename-as-path (project-dir) ".project"))
+
+(defun eval-file (file)
+  "Execute FILE and return the result of the last expression."
+
+      (with-temp-buffer
+        (insert-file-contents file)
+        (read (buffer-string))))
 
 (defun load-projects (root)
   (setf *projects* (get-dirs root)))
+
+(defun current-buffer-name ()
+   (buffer-name (current-buffer)))
+
+(defun buffer-path ()
+  (buffer-file-name (current-buffer)))
+
+
+
+
+(defun add-buffer ()
+  (interactive)
+  
+  (when (null *current-project*)                   ;; if no current-project selected
+    (error "Please, select project to proceed."))
+
+  
+  (let ((init-file (filename-as-path (project-dir) ".project")))
+    
+    (when  (not (null (file? init-file)))                        ;; if init file, eval content and delete it
+      (setf *buffers* (eval-file init-file))
+      (delete-file init-file))
+
+    (cond ((not (special-buffer (current-buffer-name)))
+	   (when (not (member (buffer-path) *buffers*)) 
+	     (push (buffer-path) *buffers*)))               ;; push buffer's path to buffers list
+	  (t
+	   (message "It is a special buffer.")))
+    
+    
+    (with-temp-file init-file                ;; writing buffers list to init file
+     ; (insert-file-contents init-file)
+      
+      (insert (pp *buffers*)))))
+
+(defun remove-buffer ()
+  (interactive)
+  (when (null *current-project*)                   ;; if no current-project selected
+    (error "Please, select project to proceed."))
+
+  
+  (let ((init-file (filename-as-path (project-dir) ".project")))
+    
+    (when (not (null (file? init-file)))                        ;; if init file, eval content and delete it
+      (setf *buffers* (eval-file init-file))
+      (delete-file init-file))
+
+    (delete (buffer-path) *buffers*)
+    
+    
+    (with-temp-file init-file                ;; writing buffers list to init file
+      ;(insert-file-contents init-file)
+      
+      (insert (pp *buffers*)))))
 
 
 (defun switch-project (project)
@@ -24,30 +86,43 @@
       (progn
 	(setf *current-project* project)
 	(kill-open-buffers)
-	(open-project-buffers project)
-	(compile-project))
-    (message "Sorry, Project does not exist")))
+	(cond ((not (null (file? (filename-as-path (project-dir) ".project"))))
+	       (setf *buffers* (eval-file (filename-as-path (project-dir) ".project")))
+	       (open-project-buffers *buffers*))
+	      (t
+	       (setf *buffers* '()))))
+    ;;	(compile-project))
+    (message (format "Sorry, Project \'%s\'  does not exist or may be Root is not set accordingly." project))))
 
-(defun compile-project ()
-  (let ((str0 (concat "(setf clws:*clws-package-path* \"" (filename-as-path (project-dir) "") "\")"))
-	(str1 (concat "(load (compile-file \"" (filename-as-path (project-dir) "init.lisp") "\"))")))
-    (cadr (slime-eval `(swank:eval-and-grab-output ,str0)))
-    (cadr (slime-eval `(swank:eval-and-grab-output ,str1)))))
+(defun s-right (idx str)
+  (let ((len (length str)))    
+    (substring str (* idx -1) len)))
 
-(defun open-project-buffers (project)
-    (open-buffers (get-files (project-dir))))
 
-(defun open-buffers (buffers-list)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defun compile-project ()										        ;;
+;;   (let ((str0 (concat "(setf clws:*clws-package-path* \"" (filename-as-path (project-dir) "") "\")"))        ;;
+;; 	(str1 (concat "(load (compile-file \"" (filename-as-path (project-dir) "init.lisp") "\"))")))	        ;;
+;;     (cadr (slime-eval `(swank:eval-and-grab-output ,str0)))						        ;;
+;;     (cadr (slime-eval `(swank:eval-and-grab-output ,str1)))))					        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun open-project-buffers (buffers-list)
   (first-nil buffers-list
 	     (progn (if (and (not (equal (s-right 3 first) "elc")) (not (equal (s-right 4 first) "fasl")))
-			(find-file-noselect (filename-as-path (project-dir) first)))
-		    (open-buffers (cdr buffers-list)))))
+			(find-file-noselect first))
+		    (open-project-buffers (cdr buffers-list)))))
 
 (defun open-files (files-list)
   (first-nil files-list
 	     (progn
 	       (find-file-noselect first)
 	       (open-files (cdr files-list)))))
+
+(defun s-trim (str)
+  (require 'subr-x)
+  (string-trim-right (string-trim-left str)))
+
 
 
 (defun get-buffers ()
@@ -70,6 +145,7 @@
 (defun special-buffer (buffer-name)
   (if (and (equal (substring buffer-name 0 1) "*") (equal (substring buffer-name -1) "*"))
       t))
+
 
 
 
