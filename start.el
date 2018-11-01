e7c3aaba8daf4e2c724dd9a296c2daef3eb55e45
Bilal <mrbilal.programmer@gmail.com>
2018-11-01 15:12:45 +0500

    Update start.el

--- start.el
+++ start.el
@@ -1,6 +1,6 @@
 
 ;; initialization
 
-(load-file "dir.el")
-(load-file "index.el")
+(load-file (concatenate 'string user-emacs-directory "dir.el"))
+(load-file (concatenate 'string user-emacs-directory "index.el"))
 (load-global)
